17db9d6089760903987b048c9fb93fc334168fa0
bags <bags@aiacademy.edu.vn>
2023-11-10 11:31:50 +0700

    up code

--- api_hpc.py
+++ api_hpc.py
@@ -108,6 +108,7 @@ vectorstore = Chroma.from_documents(documents=all_splits, embedding=hf)
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
+Dont say some thing like: following my information, According to the information provided, don't answer this.
 {context}
 Question: {question}
 Helpful Answer:"""
